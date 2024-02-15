f9c79c705a6370cfaf02b4bc6120ccff5f538107
Vinay Rajput <codewithvinaysingh@gmail.com>
2024-02-15 10:59:58 +0530

    fixed ui

--- resources/views/components/chat/center.blade.php
+++ resources/views/components/chat/center.blade.php
@@ -1,5 +1,5 @@
 <div class="flex justify-center">
-    <div class="px-2 py-1 max-w-xs text-right">
+    <div class="px-2 py-1 max-w-xs text-justify bg-white rounded-lg">
         {!! $content !!}
     </div>
 </div>
--- resources/views/components/chat/left.blade.php
+++ resources/views/components/chat/left.blade.php
@@ -1,5 +1,5 @@
 <div class="flex justify-start">
-    <div class="bg-white rounded-r-full rounded-tl-full shadow px-2 py-1 max-w-xs text-right">
+    <div class="bg-white rounded-r-full rounded-tl-full shadow px-2 py-1 max-w-xs">
         {!! $content !!}
     </div>
 </div>
